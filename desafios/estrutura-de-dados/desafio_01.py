738ac8d3b575a8f67ead226b89ab026014603ac8
FerrazVinicius96 <vesf.96@gmail.com>
2025-09-30 00:27:48 -0300

    chore: primeira versão dos desafios (DIO Python Fundamentals)

---
+++ desafios/estrutura-de-dados/desafio_01.py
@@ -0,0 +1,25 @@
+"""
+Descrição
+
+Desenvolva uma função em Python que recebe uma tupla de números inteiros e retorna a soma de todos os elementos dessa tupla. A função deve ser capaz de lidar com tuplas de qualquer tamanho,
+contanto que todos os elementos sejam números inteiros. O objetivo é praticar a manipulação de tuplas e a utilização de funções em Python.
+
+Explicação de Resolução:
+
+1. Converta cada string na "lista_strings" em um número inteiro utilizando a função "int()".
+2. Use a função "map()" para aplicar a função "int()" a cada elemento da "lista_strings".
+3. Armazene o resultado em uma variável chamada "elementos".
+"""
+
+
+def soma_tupla(tupla):
+    return sum(tupla)
+
+
+if __name__ == "__main__":
+    entrada = input()
+
+    elementos = tuple(map(int, entrada.split()))
+
+    resultado = soma_tupla(elementos)
+    print(f"A soma dos elementos da tupla é: {resultado}")
